43ebbc0ce874e379310dc04c07aa2217c1015246
Kron1749 <drakgr98@gmail.com>
2022-11-10 17:15:42 +0300

    Updated transfer tokens

--- examples/call-contract-with-token/DistributionExecutable.sol
+++ examples/call-contract-with-token/DistributionExecutable.sol
@@ -1,27 +1,103 @@
-//SPDX-License-Identifier: MIT
-pragma solidity 0.8.9;
+// SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.0;
+
+import '../abstract/Ownable.sol';
+import '../abstract/Pausable.sol';
 import { AxelarExecutable } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/executables/AxelarExecutable.sol';
 import { IAxelarGateway } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGateway.sol';
-import { IERC20 } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IERC20.sol';
 import { IAxelarGasService } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGasService.sol';
+import '../abstract/ReentrancyGuard.sol';
+import '../../node_modules/openzeppelin-solidity/contracts/interfaces/IERC20.sol';
 
-contract DistributionExecutable is AxelarExecutable {
+contract DistributionExecutable is AxelarExecutable, Ownable, Pausable, ReentrancyGuard {
     IAxelarGasService public immutable gasReceiver;
+    string public s_treasuryString;
+    address[] public s_treasuryAddr;
+    address public adminAddress; // address of the admin
+    address public operatorAddress; // address of the operator
+    address public gameToken; // you can pay with this token only
+    uint256 public gameFee;
+    mapping(uint256 => RoundInfo) public ledger; // key on roundId
+    mapping(address => uint256[]) public userRounds; // value is roundId
+    mapping(address => uint256) public userWinnings; // value is balance
+    uint8[] public brackets;
+    uint256[] public winnings;
+    uint8 public threshold;
 
-    constructor(address gateway_, address gasReceiver_) AxelarExecutable(gateway_) {
+    struct RoundInfo {
+        address playerAddress;
+        uint256 roundId;
+        uint256 amount;
+        bool updated; // default false
+        bool claimed; // default false
+    }
+
+    event NewOperatorAddress(address operator);
+    event NewGameToken(address tokenAddress);
+    event GameFeeSet(uint256 gameFee);
+    event GameEntered(uint256 roundId, address user, uint256 gameFee, uint8 bracket, uint256 amount);
+    event ResultUpdated(uint256 roundId, uint256 amount, uint8 bracket);
+    event TreasuryClaim(uint256 amount);
+    event PlayerClaimed(address player, uint256 amount);
+
+    modifier onlyAdmin() {
+        require(msg.sender == adminAddress, 'Not admin');
+        _;
+    }
+
+    modifier onlyAdminOrOperator() {
+        require(msg.sender == adminAddress || msg.sender == operatorAddress, 'Not operator/admin');
+        _;
+    }
+
+    modifier onlyOperator() {
+        require(msg.sender == operatorAddress, 'Not operator');
+        _;
+    }
+
+    modifier notContract() {
+        require(!_isContract(msg.sender), 'Contract not allowed');
+        require(msg.sender == tx.origin, 'Proxy contract not allowed');
+        _;
+    }
+
+    constructor(
+        address gateway_,
+        address gasReceiver_,
+        string  memory _treasury,
+        address _treasuryAddr,
+        address _adminAddress,
+        address _operatorAddress,
+        address _gameTokenAddress,
+        uint256 _gameFee
+    ) AxelarExecutable(gateway_) {
         gasReceiver = IAxelarGasService(gasReceiver_);
+        s_treasuryString = _treasury;
+        s_treasuryAddr.push(_treasuryAddr);
+        adminAddress = _adminAddress;
+        operatorAddress = _operatorAddress;
+        gameToken = _gameTokenAddress;
+        gameFee = _gameFee;
+    }
+
+    function test(
+        string memory destinationAddress,
+        address[] calldata destinationAddresses,
+        string memory symbol,
+        uint256 amount) external payable {
+            string memory destinationChain = 'Fantom';
+        sendToMany(destinationChain, destinationAddress, destinationAddresses, symbol, amount);
     }
 
     function sendToMany(
         string memory destinationChain,
         string memory destinationAddress,
-        address[] calldata destinationAddresses,
+        address[] memory destinationAddresses,
         string memory symbol,
         uint256 amount
-    ) external payable {
+    ) public payable {
         address tokenAddress = gateway.tokenAddresses(symbol);
-        IERC20(tokenAddress).transferFrom(msg.sender, address(this), amount);
         IERC20(tokenAddress).approve(address(gateway), amount);
         bytes memory payload = abi.encode(destinationAddresses);
         if (msg.value > 0) {
@@ -32,7 +108,7 @@ contract DistributionExecutable is AxelarExecutable {
                 payload,
                 symbol,
                 amount,
-                msg.sender
+                address(this)
             );
         }
         gateway.callContractWithToken(destinationChain, destinationAddress, payload, symbol, amount);
@@ -52,5 +128,165 @@ contract DistributionExecutable is AxelarExecutable {
         for (uint256 i = 0; i < recipients.length; i++) {
             IERC20(tokenAddress).transfer(recipients[i], sentAmount);
         }
+    }
+
+    function setOperator(address _operatorAddress) external onlyAdmin {
+        require(_operatorAddress != address(0), 'Cannot be zero address');
+        operatorAddress = _operatorAddress;
+
+        emit NewOperatorAddress(_operatorAddress);
+    }
+
+    function setBrackets(uint8[] calldata _brackets) external onlyAdmin {
+        brackets = _brackets;
+    }
+
+    function setWinnings(uint256[] calldata _winnings) external onlyAdmin {
+        winnings = _winnings;
+    }
+
+    function setThreshold(uint8 _threshold) external onlyAdmin {
+        threshold = _threshold;
+    }
+
+    function setGameFee(uint256 _gameFee) external onlyAdmin {
+        require(_gameFee != 0, 'Game cannot be free');
+        gameFee = _gameFee;
+
+        emit GameFeeSet(_gameFee);
+    }
+
+    function setGameToken(address tokenAddress) external onlyAdmin {
+        require(tokenAddress != address(0), 'Cannot be zero address');
+        gameToken = tokenAddress;
+
+        emit NewGameToken(tokenAddress);
+    }
+
+    function enterGame(uint256 _roundId) external whenNotPaused nonReentrant notContract {
+        require(_roundId != 0, 'missing RoundId');
+
+        RoundInfo storage roundInfo = ledger[_roundId];
+        if (roundInfo.playerAddress != address(0x0)) {
+            revert('existing roundId');
+        }
+
+        bool success = IERC20(gameToken).transferFrom(msg.sender, address(this), gameFee);
+
+        if (success) {
+            roundInfo.playerAddress = msg.sender;
+            roundInfo.amount = gameFee;
+            roundInfo.roundId = _roundId;
+            userRounds[msg.sender].push(_roundId);
+
+            uint256[2] memory result = setRoundResult(_roundId);
+
+            emit GameEntered(_roundId, msg.sender, gameFee, uint8(result[0]), result[1]);
+        } else {
+            revert('round was not paid for');
+        }
+    }
+
+    function claim() external whenNotPaused nonReentrant notContract {
+        uint256 claimValue = userWinnings[msg.sender];
+        if (claimValue == 0) {
+            revert('nothing to claim');
+        }
+
+        userWinnings[msg.sender] = 0;
+        for (uint256 i = 0; i < userRounds[msg.sender].length; i++) {
+            uint256 round = userRounds[msg.sender][i];
+            RoundInfo storage legerRound = ledger[round];
+            if (legerRound.updated && !legerRound.claimed) {
+                legerRound.claimed = true;
+            }
+        }
+
+        IERC20(gameToken).transfer(msg.sender, claimValue);
+        emit PlayerClaimed(msg.sender, claimValue);
+    }
+
+    function setRoundResult(uint256 _roundId) internal returns (uint256[2] memory) {
+        if (ledger[_roundId].playerAddress == address(0x0)) {
+            revert('not existing roundId');
+        }
+
+        uint256 amount = 0;
+        uint8 bracket = 100;
+        if (getPseudoRandom(_roundId + 1) <= threshold) {
+            bracket = getBracketForRound(_roundId);
+            amount = winnings[bracket];
+        }
+        RoundInfo storage roundInfo = ledger[_roundId];
+        roundInfo.amount = amount;
+        roundInfo.updated = true;
+        userWinnings[roundInfo.playerAddress] = userWinnings[roundInfo.playerAddress] + amount;
+
+        uint256[2] memory result = [bracket, amount];
+        return result;
+    }
+
+    function getBracketForRound(uint256 _roundId) internal view returns (uint8) {
+        uint8 randomNumber = getPseudoRandom(_roundId);
+        for (uint8 i = 0; i < brackets.length; i++) {
+            if (randomNumber <= brackets[i]) {
+                return i;
+            }
+        }
+        return 100;
+    }
+
+    function getPseudoRandom(uint256 _roundId) internal view returns (uint8) {
+        uint8 number = uint8(uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty))) % 100);
+        return uint8(uint256(keccak256(abi.encodePacked(number + 1, _roundId))) % 100);
+    }
+
+    function claimTreasury(uint256 value) external payable nonReentrant onlyAdmin {
+        // address[] calldata destinationAddresses
+        string memory symbol = 'aUSDC';
+        string memory destinationChain = 'Fantom';
+        string memory destinationAddress = toAsciiString(address(this));
+        address[] memory destinationAddresses = s_treasuryAddr;
+        
+        sendToMany(destinationChain, destinationAddress, destinationAddresses, symbol, value);
+
+
+        emit TreasuryClaim(value);
+    }
+
+    function toAsciiString(address x) internal pure returns (string memory) {
+        bytes memory s = new bytes(40);
+        for (uint i = 0; i < 20; i++) {
+            bytes1 b = bytes1(uint8(uint(uint160(x)) / (2**(8*(19 - i)))));
+            bytes1 hi = bytes1(uint8(b) / 16);
+            bytes1 lo = bytes1(uint8(b) - 16 * uint8(hi));
+            s[2*i] = char(hi);
+            s[2*i+1] = char(lo);            
+        }
+        return string(s);
+    }
+
+    function char(bytes1 b) internal pure returns (bytes1 c) {
+        if (uint8(b) < 10) return bytes1(uint8(b) + 0x30);
+        else return bytes1(uint8(b) + 0x57);
+    }
+    function getUserWinnings(address _address) external view returns (uint256) {
+        return userWinnings[_address];
+    }
+
+    function getUserRounds(address _address) external view returns (uint256[] memory) {
+        return userRounds[_address];
+    }
+
+    function getLegerEntryForRoundId(uint256 _roundId) external view returns (RoundInfo memory) {
+        return ledger[_roundId];
+    }
+
+    function _isContract(address account) internal view returns (bool) {
+        uint256 size;
+        assembly {
+            size := extcodesize(account)
+        }
+        return size > 0;
     }
 }
--- examples/call-contract-with-token/index.js
+++ examples/call-contract-with-token/index.js
@@ -11,71 +11,103 @@ const {
 
 const { sleep } = require('../../utils');
 const DistributionExecutable = require('../../artifacts/examples/call-contract-with-token/DistributionExecutable.sol/DistributionExecutable.json');
-const Slot = require('../../artifacts/examples/call-contract-with-token/Slot.sol/Slot.json');
 const Gateway = require('../../artifacts/@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGateway.sol/IAxelarGateway.json');
 const IERC20 = require('../../artifacts/@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IERC20.sol/IERC20.json');
+const Treasury = require('../../artifacts/examples/call-contract-with-token/Treasury.sol/Treasury.json');
 
 async function deploy(chain, wallet) {
     console.log(`Deploying Slot for ${chain.name}.`);
-    const contract = await deployContract(wallet, Slot, [
+    const treasury = await deployContract(wallet,Treasury)
+    console.log(treasury.address)
+    const contract = await deployContract(wallet, DistributionExecutable, [
         chain.gateway,
         chain.gasReceiver,
+        treasury.address,
+        treasury.address, 
         wallet.address,
         wallet.address,
         '0xc2fa98fab811b785b81c64ac875b31cc9e40f9d2',
         10,
     ]);
     chain.distributionExecutable = contract.address;
+    chain.treasury = treasury.address
     console.log(`Deployed Slot for ${chain.name} at ${chain.distributionExecutable}.`);
+    console.log(`Deployed Treasury for ${chain.name} at ${chain.treasury}.`);
 }
 
 async function test(chains, wallet, options) {
+
+
     const args = options.args || [];
     const getGasPrice = options.getGasPrice;
-    const source = chains.find((chain) => chain.name === (args[0] || 'Avalanche'));
+    const source = chains.find((chain) => chain.name === 'Avalanche');
     const destination = chains.find((chain) => chain.name === (args[1] || 'Fantom'));
     const amount = Math.floor(parseFloat(args[2])) * 1e6 || 10e6;
     const accounts = args.slice(3);
-
-    if (accounts.length === 0) accounts.push(wallet.address);
-
+    
     for (const chain of [source, destination]) {
         const provider = getDefaultProvider(chain.rpc);
         chain.wallet = wallet.connect(provider);
         chain.contract = new Contract(chain.distributionExecutable, DistributionExecutable.abi, chain.wallet);
+        chain.treasury = new Contract(chain.treasury,Treasury.abi,chain.wallet)
         chain.gateway = new Contract(chain.gateway, Gateway.abi, chain.wallet);
         const usdcAddress = chain.gateway.tokenAddresses('aUSDC');
         chain.usdc = new Contract(usdcAddress, IERC20.abi, chain.wallet);
     }
 
-    async function logAccountBalances() {
-        for (const account of accounts) {
-            console.log(`${account} has ${(await destination.usdc.balanceOf(account)) / 1e6} aUSDC`);
-        }
-    }
+    const treasuryAddress = await source.treasury.address
+    console.log(treasuryAddress)
 
-    console.log('--- Initially ---');
-    await logAccountBalances();
+    if (accounts.length === 0) accounts.push(treasuryAddress);
+    await source.usdc.transfer(source.contract.address,10000000)
+    const balanceOfTreausryBeforeSource = (await source.usdc.balanceOf(treasuryAddress))
+    const balanceOfTreausryBeforDestination = (await destination.usdc.balanceOf(treasuryAddress))
+    const balanceOnSlotBeforeSource = (await source.usdc.balanceOf(source.contract.address))
+    const balanceOnSlotBeforeDestination = (await destination.usdc.balanceOf(destination.contract.address))
+    const balanceOnWalletBeforeSource = (await source.usdc.balanceOf(wallet.address))
+    const balanceOnWalletBeforeDestination = (await destination.usdc.balanceOf(wallet.address))
+    console.log(`balanceOnTreasuryBeforeSource on ${source.name} is ${balanceOfTreausryBeforeSource}`)
+    console.log(`balanceOnTreasuryBeforeDestination on ${destination.name} is ${balanceOfTreausryBeforDestination}`)
+    console.log(`balanceOnSlotBeforeSource on ${source.name} is ${balanceOnSlotBeforeSource}`)
+    console.log(`balanceOnSlotBeforeDestination on ${destination.name} is ${balanceOnSlotBeforeDestination}`)
+    console.log(`balanceOnWalletBeforeSource on ${source.name} is ${balanceOnWalletBeforeSource}`)
+    console.log(`balanceOnWalletBeforeDestination on ${destination.name} is ${balanceOnWalletBeforeDestination}`)
+    const balance = BigInt(await destination.usdc.balanceOf(accounts[0]));
+    const approveTx = await source.usdc.approve(source.contract.address, amount);
+    await approveTx.wait();
+    // const Allowance = await source.usdc.allowance(source.contract.address,amount)
+    // await increaseAllowance.wait()
 
     const gasLimit = 3e6;
     const gasPrice = await getGasPrice(source, destination, AddressZero);
-
-    const balance = BigInt(await destination.usdc.balanceOf(accounts[0]));
-
-    const approveTx = await source.usdc.approve(source.contract.address, amount);
-    await approveTx.wait();
-
-    const sendTx = await source.contract.sendToMany(destination.name, destination.distributionExecutable, accounts, 'aUSDC', amount, {
+    // const sendTx = await source.contract.test(destination.contract.address, accounts, 'aUSDC', amount,{
+    //     value: BigInt(Math.floor(gasLimit * gasPrice)),
+    // });
+    console.log(destination.contract.address)
+    console.log(treasuryAddress)
+    const sendTx = await source.contract.claimTreasury(amount,{
         value: BigInt(Math.floor(gasLimit * gasPrice)),
-    });
+    })
+
     await sendTx.wait();
 
-    while (BigInt(await destination.usdc.balanceOf(accounts[0])) === balance) {
+       while (BigInt(await destination.usdc.balanceOf(accounts[0])) === balance) {
         await sleep(2000);
     }
 
-    console.log('--- After ---');
-    await logAccountBalances();
+    console.log("AFTER --------")
+    const balanceOfTreausryAfterSource = (await source.usdc.balanceOf(treasuryAddress))
+    const balanceOfTreausryAfterDestination = (await destination.usdc.balanceOf(treasuryAddress))
+    const balanceOnSlotAfterSource = (await source.usdc.balanceOf(source.contract.address))
+    const balanceOnSlotAfterDestination = (await destination.usdc.balanceOf(destination.contract.address))
+    const balanceOnWalletAfterSource = (await source.usdc.balanceOf(wallet.address))
+    const balanceOnWalletAfterDestination = (await destination.usdc.balanceOf(wallet.address))
+    console.log(`balanceOnTreasuryAfterSource on ${source.name} is ${balanceOfTreausryAfterSource}`)
+    console.log(`balanceOfTreausryAfterDestination on ${destination.name} is ${balanceOfTreausryAfterDestination}`)
+    console.log(`balanceOnSlotAfterSource on ${source.name} is ${balanceOnSlotAfterSource}`)
+    console.log(`balanceOnSlotAfterDestination on ${destination.name} is ${balanceOnSlotAfterDestination}`)
+    console.log(`balanceOnWalletAfterSource on ${source.name} is ${balanceOnWalletAfterSource}`)
+    console.log(`balanceOnWalletAfterDestination on ${destination.name} is ${balanceOnWalletAfterDestination}`)
 }
 
 module.exports = {
